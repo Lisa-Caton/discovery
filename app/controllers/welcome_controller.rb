2a0866dff74f94d7b1c112934c479c815aa4351b
Lisa Caton <lcaton@testsys.com>
2019-11-08 11:26:42 -0500

    user validation, pending

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,20 +4,31 @@ require 'json'
 
 class WelcomeController < ApplicationController
   def index
+    # Get Airing Today: TV Show
+    response = Excon.get("https://api.themoviedb.org/3/tv/airing_today?api_key=#{api_key}&language=en-US&page=1")
+    @tv_show_air_today = JSON.parse(response.body)['results']
+
+    # Trending People
+    response = Excon.get("https://api.themoviedb.org/3/trending/person/week?api_key=#{api_key}")
+    @trending_people = JSON.parse(response.body)['results']
+
+    # TV Show | Popular
+    response = Excon.get("https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
+    @tv_show_popular = JSON.parse(response.body)['results']
+
+    # Movies | Popular
     response = Excon.get("https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=en-US")
     @popular_movies = JSON.parse(response.body)['results']
-
-    response = Excon.get("https://api.themoviedb.org/3/tv/top_rated?api_key=#{api_key}")
-    @TR_tv_shows = JSON.parse(response.body)['results']
-
-    response = Excon.get("https://api.themoviedb.org/3/trending/person/day?api_key=#{api_key}")
-    @trending_actor = JSON.parse(response.body)['results']
   end
 
   def search
-      @query = params[:search_this_item].downcase
-      response = Excon.get("https://api.themoviedb.org/3/search/multi?api_key=#{api_key}&language=en-US&query=#{@query}")
-      @search_data = JSON.parse(response.body)
+    # Everything (Multi) | Search
+    @query = params[:search_this_item].downcase
+    if @query == "!"
+      flash[:alert] = "No"
+    end
+    response = Excon.get("https://api.themoviedb.org/3/search/multi?api_key=#{api_key}&language=en-US&query=#{@query}")
+    @search_data = JSON.parse(response.body)['results']
   end
 
 
